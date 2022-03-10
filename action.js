4b62ddf30ab8aafcf2765b371dc9456579e520b5
Daman30028646 <30028646@student.toiohomai.ac.nz>
2022-03-11 10:39:06 +1300

    json parasing

--- action.js
+++ action.js
@@ -1,4 +1,4 @@
-$('.reply').click(function(){
+$(document).on('click','.reply', function(){
     var id = this.id;
     var message = $('.message');
     
@@ -21,7 +21,7 @@ $('.reply').click(function(){
     
 });
 
-$('.edit').click(function(){
+$(document).on('click','.edit', function(){
     var id = this.id;
     var message = $('.message');
     if(!$(this).hasClass('active')){
@@ -42,17 +42,17 @@ $('.edit').click(function(){
     }
 });
 
-$('.delete').click(function(){
+$(document).on('click','.delete', function(){
     var id = this.id;
     $('.deleteBtn').attr('data-id',id);
     $('.modal').toggleClass('opened');
 });
 
-$('.cancel').click(function(){
+$(document).on('click','.cancel', function(){
     $('.modal').toggleClass('opened');
     $('.deleteBtn').removeAttr('data_id');
 })
-$('.deleteBtn').click(function(){
+$(document).on('click','.deleteBtn', function(){
     var id = $(this).attr('data-id');
     var message = $('.message');
     message.each(function(){
@@ -73,6 +73,92 @@ $('.deleteBtn').click(function(){
 
 $.getJSON("./data.json", function(json) {
     var currentUser = json.currentUser;
+    var username =  currentUser.username;
     var comments = json.comments;
     $('.chatreply .avatar img').attr('src',currentUser.image.png);
+    var html = '';
+    $.each(comments,function(i, comment){
+        var html1= '<div class="message" data-id="'+comment.id+'">\
+                    <div class="vote">\
+                        <span class="plus"></span>\
+                        <span class="votes">'+comment.score+'</span>\
+                        <span class="minus"></span>\
+                    </div>\
+                    <div class="contains">\
+                        <div class="head">\
+                            <div class="left">\
+                                <div class="avatar">\
+                                    <img src="'+comment.user.image.png+'" alt="Default avatar ">\
+                                </div>\
+                                <span class="name">'+comment.user.username;
+                                if(username == comment.user.username){
+                                    html1 += '<span class="you">you</span></span>';
+                                }
+                                html1 +=' <span class="time">'+comment.createdAt+'</span>\
+                            </div>\
+                            <div class="right">';
+                            if(username == comment.user.username){
+                                html1 += '<a href="#" class="actions delete" id="'+comment.id+'"><span></span> Delete</a><a href="#" class="actions edit" id="'+comment.id+'"><span></span> Edit</a>';
+                            }
+                            else{
+                                html1 += '<a href="#" class="actions reply" id="'+comment.id+'"><span></span> Reply</a>';
+                            }
+                            html1 += '</div>\
+                        </div>\
+                        <div class="body">\
+                            <p class="messageContain">'+comment.content+'</p>\
+                        </div>\
+                    </div>\
+                </div>';
+        if(comment.replies.length != 0){
+        html += '<div class="multiple-message">'+html1;
+        html +=     '<div class="neted-message" data-id="'+comment.id+'">\
+                        <div class="vl"></div>\
+                        <div class="messages">';
+            var replies = comment.replies;
+            $.each(replies,function(i, reply){
+                    html += '<div class="message" data-id="'+reply.id+'">\
+                                <div class="vote">\
+                                    <span class="plus"></span>\
+                                    <span class="votes">'+reply.score+'</span>\
+                                    <span class="minus"></span>\
+                                </div>\
+                                <div class="contains">\
+                                    <div class="head">\
+                                        <div class="left">\
+                                            <div class="avatar">\
+                                                <img src="'+reply.user.image.png+'" alt="Default avatar ">\
+                                            </div>\
+                                            <span class="name">'+reply.user.username;
+                                            if(username == reply.user.username){
+                                                html += '<span class="you">you</span></span>';
+                                            }
+                                            html +=' <span class="time">'+reply.createdAt+'</span>\
+                                        </div>\
+                                        <div class="right">';
+                                        if(username == reply.user.username){
+                                            html += '<a href="#" class="actions delete" id="'+reply.id+'"><span></span> Delete</a><a href="#" class="actions edit" id="'+reply.id+'"><span></span> Edit</a>';
+                                        }
+                                        else{
+                                            html += '<a href="#" class="actions reply" id="'+reply.id+'"><span></span> Reply</a>';
+                                        }
+                                        html += '</div>\
+                                    </div>\
+                                    <div class="body">\
+                                        <p class="messageContain">'+reply.content+'</p>\
+                                    </div>\
+                                </div>\
+                            </div>';
+            });
+        html +=         '</div>\
+                    </div>\
+                </div>'; 
+        }
+        else{
+            html += html1;
+        }
+        
+    });
+
+    $('.chatting').html(html);
 });
