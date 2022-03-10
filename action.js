9d50890d22c5f65d1099b60798cda43627778182
Daman30028646 <30028646@student.toiohomai.ac.nz>
2022-03-11 08:52:25 +1300

    edit event

--- action.js
+++ action.js
@@ -19,4 +19,26 @@ $('.reply').click(function(){
         $(this).toggleClass('active');
     }
     
-})
+});
+
+$('.edit').click(function(){
+    var id = this.id;
+    var message = $('.message');
+    if(!$(this).hasClass('active')){
+        message.each(function(){
+            var data_id = $(this).attr('data-id');
+            
+            if(id == data_id){
+                var messageContain = $(this).find('div.contains div.body p.messageContain');
+                
+                var data = messageContain.text();
+                var html = '<textarea name="" data-id="'+id+'" id="" class="addComment editcomment">'+data+'</textarea><button class="send update" data-id="'+id+'">Update</button>';
+                messageContain.after(html);
+                messageContain.css('visibility','hidden');
+                messageContain.css('height','0');
+            }
+        });
+        $(this).toggleClass('active');
+    }
+});
+
